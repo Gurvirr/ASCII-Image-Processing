3290e2ec5cc38e6a88ab9a00ae84f4f0372fc6ed
Gurvir <gurvirrandhawa28@gmail.com>
2025-06-08 17:19:31 -0400

    feat: focus on command input when side nav is open by default

--- static/js/side-nav-toggle.js
+++ static/js/side-nav-toggle.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.key === '/' && event.target !== commandInput) { // Only toggle nav if not typing in command input
             event.preventDefault(); 
             document.body.classList.toggle('side-nav-is-open');
+            if (document.body.classList.contains('side-nav-is-open')) {
+                commandInput.focus();
+            }
         }
     });
 
