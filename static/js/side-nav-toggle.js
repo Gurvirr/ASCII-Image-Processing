5889a788c979c50f1d6b4f5ad576569a9694406b
Gurvir <gurvirrandhawa28@gmail.com>
2025-06-24 12:00:00 -0400

    refactor

--- static/js/side-nav-toggle.js
+++ static/js/side-nav-toggle.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const commandInput = document.getElementById('command-input');
-    const commandOutputContainer = document.getElementById('command-output-container');
+document.addEventListener("DOMContentLoaded", () => {
+    const commandInput = document.getElementById("command-input");
+    const commandOutputContainer = document.getElementById("command-output-container");
 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === '/' && event.target !== commandInput) { // Only toggle nav if not typing in command input
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "/" && event.target !== commandInput) { // Only toggle nav if not typing in command input
             event.preventDefault(); 
-            document.body.classList.toggle('side-nav-is-open');
-            if (document.body.classList.contains('side-nav-is-open')) {
+            document.body.classList.toggle("side-nav-is-open");
+            if (document.body.classList.contains("side-nav-is-open")) {
                 commandInput.focus();
             }
         }
