54fe19718d8131b9cba378f21c5972f3d8a3e4bc
Gurvir <gurvirrandhawa28@gmail.com>
2025-08-12 15:02:58 -0400

    feat: add 'esc' key input

--- static/js/side-nav-toggle.js
+++ static/js/side-nav-toggle.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const commandInput = document.getElementById("command-input");
-    const commandOutputContainer = document.getElementById("command-output-container");
+  const commandInput = document.getElementById("command-input");
+  const commandOutputContainer = document.getElementById(
+    "command-output-container",
+  );
 
-    document.addEventListener("keydown", function(event) {
-        if (event.key === "/" && event.target !== commandInput) { // Only toggle nav if not typing in command input
-            event.preventDefault(); 
-            document.body.classList.toggle("side-nav-is-open");
-            if (document.body.classList.contains("side-nav-is-open")) {
-                commandInput.focus();
-            }
-        }
-    });
-}); 
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "/" && event.target !== commandInput) {
+      // Only toggle nav if not typing in command input
+      event.preventDefault();
+      document.body.classList.toggle("side-nav-is-open");
+      if (document.body.classList.contains("side-nav-is-open")) {
+        commandInput.focus();
+      }
+    }
+
+    if (event.key === "Escape") {
+      document.body.classList.remove("side-nav-is-open");
+      commandInput.blur();
+    }
+  });
+});
